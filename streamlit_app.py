205832fa86bcc826549d389c5dfbda4fa00018d6
snowflake-norbertfarkas <113424541+snowflake-norbertfarkas@users.noreply.github.com>
2022-09-13 17:45:11 +0200

    Update streamlit_app.py
    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
 
 streamlit.title('My parents New Healthy Diner')
 
@@ -15,6 +18,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.dataframe(my_fruit_list)
 
