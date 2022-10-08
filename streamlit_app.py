3815f158d8bd2d34f84d335a5112f4fe3e926f7d
snowflake-norbertfarkas <113424541+snowflake-norbertfarkas@users.noreply.github.com>
2022-10-08 17:02:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,10 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
 # write your own comment -what does the next line do? 
 # this line is expanding json to a more viewable format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
