bc22ccaad3b442e28a5b14f741eb79c13528b8da
snowflake-norbertfarkas <113424541+snowflake-norbertfarkas@users.noreply.github.com>
2022-11-29 16:08:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -39,3 +39,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 # structured data is loaded into a table
 streamlit.dataframe(fruityvice_normalized)
+import snowflake.connector
