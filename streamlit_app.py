e37711894a02e7aea9a112bef7ccd2e131641159
snowflake-norbertfarkas <113424541+snowflake-norbertfarkas@users.noreply.github.com>
2022-11-29 16:38:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,13 +40,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # structured data is loaded into a table
 streamlit.dataframe(fruityvice_normalized)
 
-
-#!/usr/bin/env python
-import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
+python --version
