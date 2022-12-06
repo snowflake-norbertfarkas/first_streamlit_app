b1e55e02e9120f3aab510eee5842db7a24edc9bc
snowflake-norbertfarkas <113424541+snowflake-norbertfarkas@users.noreply.github.com>
2022-12-06 07:39:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ except URLError as e:
     streamlit.error()
 
 
-GetFacebookInformation = yahooFinance.Ticker("FB")
-streamlit.header(pandas.json_normalize(GetFacebookInformation.info.json()))
+# GetFacebookInformation = yahooFinance.Ticker("FB")
+# streamlit.header(pandas.json_normalize(GetFacebookInformation.info.json()))
 
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
